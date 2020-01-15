36118b67b3da453fdcc7506dd0f72c6e7750a1fc
CAM603 <cameronhawley@comcast.net>
2020-01-15 11:50:35 -0500

    working on darkmode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+
+}
+
+const [someValue, setSomevalue] = useLocalStorage('a key');
+
+useEffect(() => {
+
+}, [])

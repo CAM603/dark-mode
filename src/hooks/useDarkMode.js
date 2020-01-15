ead00e9300499cc373e0ace930836d0985dd34b8
CAM603 <cameronhawley@comcast.net>
2020-01-15 15:02:10 -0500

    added darkmode hook in nav

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = () => {
+export const useDarkMode = (key) => {
+    const [someValue, setSomevalue] = useLocalStorage(key);
 
+    useEffect(() => {
+        let body = document.querySelector('body');
+
+        someValue ? body.classList.add('dark-mode') 
+        : body.classList.remove('dark-mode')
+        
+    }, [someValue])
+
+    return [someValue, setSomevalue]
 }
-
-const [someValue, setSomevalue] = useLocalStorage('a key');
-
-useEffect(() => {
-
-}, [])

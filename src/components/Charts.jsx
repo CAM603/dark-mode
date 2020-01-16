a00492e5b56101da4dcf19f6c2207d6d2f801d77
CAM603 <cameronhawley@comcast.net>
2020-01-16 17:29:31 -0500

    fixed key

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -6,8 +6,8 @@ const Charts = ({ coinData, darkMode }) => {
   return (
     <div className="charts">
       {coinData.map(coin => (
-        <Link to={`/${coin.id}`}>
-          <div className="chart__container" key={coin.name}>
+        <Link to={`/${coin.id}`} key={coin.name}>
+          <div className="chart__container">
             <h2 className="coin__title">{coin.name}</h2>
             <h4 className="coin__symbol">{coin.symbol}</h4>
             <div className="coin__logo">
